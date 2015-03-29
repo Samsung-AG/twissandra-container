f1100571fb262883f20c67f7b7f0dcc49835bd48
Mikel Nelson <github@mikelnelson.net>
2015-03-29 08:07:48 -0700

    check different docker image build

---
+++ kubernetes/web.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Twissandra web app start
+# 3/13/2015 mikeln
+#
+# This attempts to determine if we are starting in Kubernetes or not.
+# If so, read the needed Kubernetes env vars and store the information in /etc/hosts
+# if not, assume we are running in docker only...do the docker only setup (nothing in this case) 
+#
+# any app just needs to locate the cassandra kubernetes service entrypoint
+#
+echo "Twissandra web script start"
+
+#URDIR="${BASH_SOURCE%/*}"
+#if [[ ! -d "$CURDIR" ]]; then CURDIR="$PWD"; fi
+#. "$CURDIR/twiss-start"
+#. "$CURDIR/init.sh"
+. twiss-start
+
+# Start the app
+# NOTE: need to supply the args for this...
+echo Starting Twissandra web...
+#
+# pass the input args to the python thing...
+#
+python /twissandra/manage.py runserver

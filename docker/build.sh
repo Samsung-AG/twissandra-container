aa8b896c2f66b74d1a11f901f97c6766d0d91767
Mikel Nelson <github@mikelnelson.net>
2015-04-09 16:51:47 -0700

    modify to checkout from git vs baking in

--- docker/build.sh
+++ docker/build.sh
@@ -14,13 +14,66 @@ git clone git@github.com:mikeln/twissandra.git /twissandra
 
 #
 # hardcode the cass for now
-echo "10.247.87.50 cass" >> /etc/hosts
+#echo "10.247.81.229 cass" >> /etc/hosts
+INKUB=`env | grep ^KUBERNETES_RO`
+if [ -n "$INKUB" ]; then
+    # in kubernetes
+    echo "Running inside Kubernetes"
+    #
+    # note: this is the expect hostname in the app: cass
+    #
+    CASSHOSTNAME="cass"
+    #
+    # find the cassandra service: we only need the host IP... ports are a given 9042, 9160
+    #
+    CASSIP=`env | grep CASSANDRA_SERVICE_HOST | cut -d "=" -f 2`
+    if [ -n "$CASSIP" ]; then
+        echo "Found Cassandra Service at IP: $CASSIP"
+        #
+        # simulate DOCKER by adding to /etc/hosts file
+        #
+        echo "$CASSIP $CASSHOSTNAME" >> /etc/hosts
+        echo "hosts change ------------------"
+        cat /etc/hosts
+        echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    else
+        echo "WARNING no cassandra kubernetes service info was found.  Is it running?"
+#        exit 1
+    fi
+else
+    echo "Running inside Docker only...nothing to do"
+fi
 
 # Get pip to download and install requirements:
 pip install -r /twissandra/requirements.txt
 
 cd /twissandra
 
-python manage.py runserver
+if [ $# -lt 1 ]; then
+   python manage.py 
+else
+    if [ "$1" = "db" ]; then
+        echo "do db thing"
+        python manage.py sync_cassandra
+    elif [ "$1" = "er" ]; then
+        echo "erase thing"
+        python manage.py sync_cassandra y
+    elif [ "$1" = "app" ]; then
+        echo "server thing"
+        if [ -n "$2" ]; then
+            echo "running server at $2"
+            python manage.py runserver $2
+        else
+            echo "running server at default 0.0.0.0:8222"
+            python manage.py runserver 0.0.0.0:8222
+        fi
+    elif [ "$1" = "inj" ]; then
+        echo "script inject thing"
+        python manage.py inject_data 10 10 0 0
+    else
+        echo "unknown args"
+    fi
+fi    
 
-tail -f /var/log/lastlog
+
+#tail -f /var/log/lastlog
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,9 @@
 #/bin/bash
-
+#
+# DEPRECATED... original run setup... ONLY HERE FOR REFERENCE 4/9/2015 MLN
+#
+# INCORRECT, but gives a hint how to run cass and twiss by hand
+#
 #docker run -it --rm --name twiss_init --link cass1:cass mikeln/twissandra_db_img
 #docker run --rm --name twiss_inj --link cass1:cass mikeln/twissandra_inj_img 10 10 0 0
 #docker run --rm --name twis_app -p 8000:8000 --link cass1:cass mikeln/twissandra_app_img
